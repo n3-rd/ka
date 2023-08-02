4ead259ce65d2bc309bff3aea2ffaaf930283614
n3-rd <godwinjdayo@gmail.com>
2023-08-02 04:09:55 +0100

    built layout

---
+++ src/lib/Sidebar.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	let spanClass = 'flex-1 ml-3 whitespace-nowrap';
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<Sidebar asideClass="h-screen">
+	<SidebarWrapper>
+		<SidebarGroup>
+			<SidebarItem>
+				<a href="/" class="flex items-center">qw</a>
+			</SidebarItem>
+		</SidebarGroup>
+	</SidebarWrapper>
+</Sidebar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,63 @@
 <script>
+	import Navbar from '$lib/Navbar.svelte';
+	import Sidebar from '$lib/Sidebar.svelte';
 	import '../app.postcss';
 </script>
 
-<slot />
+<main>
+	<div class="wrapper">
+		<!-- Sidebar -->
+		<aside class="sidebar h-screen bg-red-600">
+			<Sidebar />
+		</aside>
+
+		<div class="content bg-slate-50">
+			<!-- Navbar -->
+			<nav class="navbar">
+				<!-- Your navbar content here -->
+				<Navbar />
+			</nav>
+
+			<!-- Main content area -->
+			<div class="main-content">
+				<!-- Your main content here -->
+				<slot />
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	/* Style your layout here */
+
+	.wrapper {
+		display: flex;
+		height: 100vh; /* Adjust height as needed */
+	}
+
+	.sidebar {
+		/* Define styles for the sidebar */
+		flex: 0 0 250px; /* Adjust width as needed */
+		background-color: #f0f0f0;
+		/* Other styles... */
+	}
+
+	.content {
+		/* Define styles for the content area */
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.navbar {
+		/* Define styles for the navbar */
+
+		/* Other styles... */
+	}
+
+	.main-content {
+		/* Define styles for the main content area */
+		padding: 1rem;
+		/* Other styles... */
+	}
+</style>
